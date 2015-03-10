f3c2627e605e9e38584fb88d348849382378b884
GabrielDuque <gabrielferrazduque@gmail.com>
2015-03-09 21:52:13 -0300

    Update historian.py

--- historian.py
+++ historian.py
@@ -46,7 +46,8 @@ import Android/ART
 import Chrome/ChromeOS 
 import Linux/ChromeOS 
 import OS X/Chrome
-import ios/Chrome
+import ios/Chrome 
+import Privacy/ART/Android
 POWER_DATA_FILE_TIME_OFFSET = 0  # deal with any clock mismatch.
 BLAME_CATEGORY = "wake_lock_in"  # category to assign power blame to.
 ROWS_TO_SUMMARIZE = ["wake_lock", "running"]  # -s: summarize these rows
